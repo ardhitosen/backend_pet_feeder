a57ea52b97cfd7bc6cab86a18db79f2a947a0ead
David Ongky <da.ongky88@gmail.com>
2023-10-11 10:45:56 +0700

    Connect Database

--- main.py
+++ main.py
@@ -1,8 +1,70 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+models.Base.metadata.create_all(bind = engine)
+
+from pydantic import BaseModel
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class UserCreate(BaseModel):
+    nama: str
+    device: str
+    jumlah_hewan: int
+
+class User(UserCreate):
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+class PetBase(BaseModel):
+    nama: str
+    berat: int
+    porsi_makan: int
+    tipe_hewan: str
+    ras_hewan: str
+    umur: int
+
+class PetCreate(PetBase):
+    pass
+
+class Pet(PetBase):
+    pet_id: int
+    user_id: int
+    device_id: int
+
+    class Config:
+        orm_mode = True
+
+class DeviceBase(BaseModel):
+    models: str
+
+class DeviceCreate(DeviceBase):
+    pass
+
+class Device(DeviceBase):
+    device_id: int
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/user/", status_code = status.HTTP_201_CREATED)
+async def create_user(user: UserCreate, db: db_dependency):
+    db_user = models.User(**user.dict())
+    db.add(db_user)
+    db.commit()
---
+++ models.py
@@ -0,0 +1,43 @@
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from database import Base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    nama = Column(String(50))
+    device = Column(String(50), nullable = True)
+    jumlah_hewan = Column(Integer)
+
+    # Define the relationship with pets and devices
+    pets = relationship("Pet", back_populates="owner")
+    devices = relationship("Device", back_populates="owner")
+
+class Pet(Base):
+    __tablename__ = 'pets'
+
+    pet_id = Column(Integer, primary_key=True, index=True)
+    nama = Column(String(50))
+    berat = Column(Integer)
+    porsi_makan = Column(Integer)
+    tipe_hewan = Column(String(30))
+    ras_hewan = Column(String(30))
+    umur = Column(Integer)
+    user_id = Column(Integer, ForeignKey("user.user_id"))
+
+    # Define the relationship with users
+    owner = relationship("User", back_populates="pets")
+
+class Device(Base):
+    __tablename__ = 'devices'
+
+    device_id = Column(Integer, primary_key=True, index=True)
+    model = Column(String(30))
+    user_id = Column(Integer, ForeignKey("user.user_id"))
+
+    # Define the relationship with users
+    owner = relationship("User", back_populates="devices")
