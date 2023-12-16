ce0b2d51d4175718ba504ab3b2473b19192dec34
ardhitosen <christoforusardhito@gmail.com>
2023-12-16 18:08:16 +0700

    mengganti post /login`

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ async def create_user(user: UserCreate, db: db_dependency):
 
 @app.post("/login")
 def login_user(user: UserCredentials, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.nama == user.nama).first()
+    db_user = db.query(models.User).filter(models.User.full_name == user.nama).first()
     if db_user is None or db_user.password != user.password:
         raise HTTPException(status_code=400, detail="Invalid credentials")
     return db_user
